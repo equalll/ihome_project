e9ca6f17ad2d04c8b6e00986aae5ca9e841cef6e
lgwjwl <lgwjw@qq.com>
2018-04-14 21:29:32 +0800

    代码抽取

--- ihome/manage.py
+++ ihome/manage.py
@@ -7,45 +7,12 @@
 # @Software: PyCharm
 
 # -*- coding:utf-8 -*-
-import redis
-from flask import Flask
 from flask.ext.migrate import Migrate, MigrateCommand
 from flask.ext.script import Manager
-from flask_session import Session
-from flask_sqlalchemy import SQLAlchemy
-from flask_wtf.csrf import CSRFProtect
+from ihome import create_app,db
 
-app = Flask(__name__)
-
-class Config(object):
-    """工程配置信息"""
-    SECRET_KEY = "lgwjwl"
-
-    DEBUG = True
-
-    # 数据库配置信息
-    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/ihome"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    # Redis配置
-    REDIS_HOST = "127.0.0.1"
-    REDIS_PORT = 6379
-
-    # flask_session配置信息
-    SESSION_TYPE = "redis"
-    SESSION_USE_SIGNER = True
-    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST,port=REDIS_PORT)
-    PERMANENT_SESSION_LIFETIME = 86400  # session 的有效期，单位是秒
-
-
-
-
-
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
-Session(app)
-csrf = CSRFProtect(app)
+# 创建 app，并传入配置模式：development / production
+app = create_app("development")
 manager = Manager(app)
 Migrate(app,db)
 manager.add_command('db',MigrateCommand)
