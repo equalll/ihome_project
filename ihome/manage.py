4f119c70952ed73524ab4389fef4fac25f46057e
lgwjwl <lgwjw@qq.com>
2018-04-14 19:23:50 +0800

    配置Reids

--- ihome/manage.py
+++ ihome/manage.py
@@ -7,10 +7,29 @@
 # @Software: PyCharm
 
 # -*- coding:utf-8 -*-
-
+import redis
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+class Config(object):
+    """工程配置信息"""
+    DEBUG = True
+
+    # 数据库配置信息
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/ihome"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Redis配置
+    REDIS_HOST = "127.0.0.1"
+    REDIS_PORT = 6379
+
+
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
+
 
 @app.route('/index')
 def index():
